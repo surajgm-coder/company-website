26bf31b87a7d1bff13642a33a61ab6789b74c092
surajgm-coder <suraj.gm@nkenters.com>
2025-12-25 16:56:22 +0530

    Create cards.js

---
+++ js/cards.js
@@ -0,0 +1,21 @@
+// js/cards.js - Optional 3D tilt on cards
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".card.3d");
+
+  cards.forEach(card => {
+    card.addEventListener("mousemove", (e) => {
+      const rect = card.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      const centerX = rect.width / 2;
+      const centerY = rect.height / 2;
+      const rotateY = ((x - centerX) / centerX) * 15;
+      const rotateX = ((centerY - y) / centerY) * 15;
+      card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`;
+    });
+
+    card.addEventListener("mouseleave", () => {
+      card.style.transform = "perspective(1000px) rotateX(0) rotateY(0) scale(1)";
+    });
+  });
+});
